0cecb8b65fa030bcfb22c80fdd533c775f6d0070
Kenn Pascascio <kenn370@gmail.com>
2018-10-23 15:09:53 -0400

    added cors

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 require('./models');
 
+app.use(require('cors')())
+
 app.listen(process.env.PORT, () => {
     console.log(`server is listening on port ${process.env.PORT}`);
 })
