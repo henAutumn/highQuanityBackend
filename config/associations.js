3dcf685d6fd746dabfa327fc94bc6d9f24625a2b
davidwhitt00 <39777013+davidwhitt00@users.noreply.github.com>
2018-10-24 11:20:41 -0400

    newdave

--- config/associations.js
+++ config/associations.js
@@ -2,7 +2,7 @@ const sequelize = require('../models').sequelize;
 const Product = sequelize.model('product'); 
 const User = sequelize.model('user');
 const Transactions = sequelize.import('../models/transaction')
-Transactions.sync({force:true})
+//Transactions.sync({force:true})
 User.hasMany(Product);
 Product.belongsTo(User);
 
